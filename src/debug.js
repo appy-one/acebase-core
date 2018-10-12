328105764811d2ab0213615cf2a75c4667c3ce86
Ewout Stortenbeker <me@appy.one>
2018-10-12 09:21:54 +0200

    Fixed debug.setLevel

--- src/debug.js
+++ src/debug.js
@@ -1,11 +1,10 @@
-let LOG_LEVEL = "log";
 const debug = {
-    log: ["log"].indexOf(LOG_LEVEL) >= 0 ? console.log.bind(console) : ()=>{},
-    warn: ["log", "warn"].indexOf(LOG_LEVEL) >= 0  ? console.warn.bind(console) : ()=>{},
-    error: ["log", "warn", "error"].indexOf(LOG_LEVEL) >= 0 ? console.error.bind(console) : ()=>{},
     setLevel(level) {
-        LOG_LEVEL = level;
+        this.log = ["log"].indexOf(level) >= 0 ? console.log.bind(console) : ()=>{};
+        this.warn = ["log", "warn"].indexOf(level) >= 0 ? console.warn.bind(console) : ()=>{};
+        this.error = ["log", "warn", "error"].indexOf(level) >= 0 ? console.error.bind(console) : ()=>{};
     }
 };
+debug.setLevel("log"); // default
 
 module.exports = debug;

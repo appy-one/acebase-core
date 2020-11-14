80dfc45a21b1872f3703119cc0fe2fc787978047
Ewout Stortenbeker <me@appy.one>
2020-11-14 08:50:42 +0100

    optional log prefixing

--- src/debug.js
+++ src/debug.js
@@ -6,10 +6,10 @@ class DebugLogger {
     setLevel(level) {
         const prefix = this.prefix ? this.prefix : '';
         this.level = level;
-        this.verbose = ["verbose"].includes(level) ? console.log.bind(console, prefix) : () => {};
-        this.log = ["verbose", "log"].includes(level) ? console.log.bind(console, prefix) : () => {};
-        this.warn = ["verbose", "log", "warn"].includes(level) ? console.warn.bind(console, prefix) : () => {};
-        this.error = ["verbose", "log", "warn", "error"].includes(level) ? console.error.bind(console, prefix) : () => {};
+        this.verbose = ["verbose"].includes(level) ? prefix ? console.log.bind(console, prefix) : console.log.bind(console) : () => {};
+        this.log = ["verbose", "log"].includes(level) ? prefix ? console.log.bind(console, prefix) : console.log.bind(console) : () => {};
+        this.warn = ["verbose", "log", "warn"].includes(level) ? prefix ? console.warn.bind(console, prefix) : console.warn.bind(console) : () => {};
+        this.error = ["verbose", "log", "warn", "error"].includes(level) ? prefix ? console.error.bind(console, prefix) : console.error.bind(console) : () => {};
         this.write = console.log.bind(console);
     }
 }

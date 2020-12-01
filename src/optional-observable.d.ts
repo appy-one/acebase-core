2b52a823bfc47da258dae357510ef3e27d327192
Ewout Stortenbeker <me@appy.one>
2020-12-01 13:00:44 +0100

    Export Observable type instead of implementation

--- src/optional-observable.d.ts
+++ src/optional-observable.d.ts
@@ -1,1 +1,4 @@
-export { Observable } from 'rxjs'; // An error here is ok, rxjs installation is optional
+export type { Observable } from 'rxjs'; // Typescript 3.8+ type export. Error here? Read below..
+// Add RxJS to your project if you use functions that return an Observable
+// If you using an older (<3.8) typescript version: update it, or comment out above export and enable:
+// export type Observable<T> = any;

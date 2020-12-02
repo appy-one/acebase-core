68b02659c050264fcbd8263590ae199eb6d836b2
Ewout Stortenbeker <me@appy.one>
2020-12-02 16:23:26 +0100

    Fix for RunKit console.log issue

--- src/debug.js
+++ src/debug.js
@@ -10,7 +10,11 @@ class DebugLogger {
         this.log = ["verbose", "log"].includes(level) ? prefix ? console.log.bind(console, prefix) : console.log.bind(console) : () => {};
         this.warn = ["verbose", "log", "warn"].includes(level) ? prefix ? console.warn.bind(console, prefix) : console.warn.bind(console) : () => {};
         this.error = ["verbose", "log", "warn", "error"].includes(level) ? prefix ? console.error.bind(console, prefix) : console.error.bind(console) : () => {};
-        this.write = console.log.bind(console);
+        this.write = (text) => {
+            const isRunKit = typeof process !== 'undefined' && process.env && typeof process.env.RUNKIT_ENDPOINT_PATH === 'string';
+            if (isRunKit) { text = text.replace(/^/gm, '>'); } // Fixes runkit crash with many leading spaces
+            console.log(text);
+        };
     }
 }
 

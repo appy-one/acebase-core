94ddb0a861f4956244754c3eefc3bcfe1eb03d45
Ewout Stortenbeker <4ewout@gmail.com>
2021-07-14 22:14:07 +0200

    Added @ts-ignore for optional rxjs dependency

--- types/optional-observable.d.ts
+++ types/optional-observable.d.ts
@@ -1,18 +1,6 @@
-export type { Observable } from 'rxjs'; // Typescript 3.8+ type export. Error here? Read below..
+// Optional dependency on rxjs package. If rxjs is installed into your project, you'll get the correct 
+// typings for AceBase methods that use Observables, and you'll be able to use them. If you don't use
+// those methods, there is no need to install rxjs. 
 
-/*
-How to handle a TS error in above export:
-
-- If you use AceBase functions that return Observables*, make sure you have RxJS installed: 
-npm install rxjs
-
-- If you have RxJS installed and use typescript < 3.8: try updating it, or use:
+// @ts-ignore
 export { Observable } from 'rxjs';
-
-- If you don't use AceBase functions that return Observables, replace the export with:
-export type Observable<T> = any;
-
-* AceBase function that use RxJS Observables are:
-    - ref.observe(),
-    - proxy values' getObservable()
-*/

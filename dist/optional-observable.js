0a17cf028922d7ada5515ee735fb9e43c7ccd030
Ewout Stortenbeker <4ewout@gmail.com>
2021-07-16 15:57:13 +0200

    Implemented simple Observable shim for test suite

--- dist/optional-observable.js
+++ dist/optional-observable.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.setObservable = exports.getObservable = void 0;
+exports.ObservableShim = exports.setObservable = exports.getObservable = void 0;
 let _observable;
 function getObservable() {
     if (_observable) {
@@ -24,7 +24,56 @@ function getObservable() {
 }
 exports.getObservable = getObservable;
 function setObservable(Observable) {
+    if (Observable === 'shim') {
+        console.warn(`Using AceBase's simple Observable shim. Only use this if you know what you're doing.`);
+        Observable = ObservableShim;
+    }
     _observable = Observable;
 }
 exports.setObservable = setObservable;
+;
+;
+/**
+ * rxjs is an optional dependency that only needs installing when any of AceBase's observe methods are used.
+ * In this test suite Observables are therefore not available, so we have to provide a shim
+ */
+class ObservableShim {
+    constructor(create) {
+        // private _emit() {}
+        this._active = false;
+        this._subscribers = [];
+        this._create = create;
+    }
+    subscribe(subscriber) {
+        if (!this._active) {
+            const next = (value) => {
+                // emit value to all subscribers
+                this._subscribers.forEach(s => {
+                    try {
+                        s(value);
+                    }
+                    catch (err) {
+                        console.error(`Error in subscriber callback:`, err);
+                    }
+                });
+            };
+            const observer = { next };
+            this._cleanup = this._create(observer);
+            this._active = true;
+        }
+        this._subscribers.push(subscriber);
+        const unsubscribe = () => {
+            this._subscribers.splice(this._subscribers.indexOf(subscriber), 1);
+            if (this._subscribers.length === 0) {
+                this._active = false;
+                this._cleanup();
+            }
+        };
+        const subscription = {
+            unsubscribe
+        };
+        return subscription;
+    }
+}
+exports.ObservableShim = ObservableShim;
 //# sourceMappingURL=optional-observable.js.map
--- src/optional-observable.ts
+++ src/optional-observable.ts
@@ -15,6 +15,68 @@ export function getObservable() {
         throw new Error(`RxJS Observable could not be loaded. If you are using a browser build, add it to AceBase using db.setObservable. For node.js builds, add it to your project with: npm i rxjs`);
     }
 }
+
 export function setObservable(Observable: any) {
+    if (Observable === 'shim') {
+        console.warn(`Using AceBase's simple Observable shim. Only use this if you know what you're doing.`)
+        Observable = ObservableShim;
+    }
     _observable = Observable;
+}
+
+export interface ISubscription {
+    unsubscribe(): any
+};
+interface IObserver<T> {
+    next(value: T): any
+    start?(subscription: ISubscription): void
+    error?(error: any): any
+    complete?(value: any): void
+};
+type CleanupFunction = () => any;
+type CreateFunction<T> = (observer: IObserver<T>) => CleanupFunction;
+type SubscribeFunction<T> = (value: T) => any;
+export interface IObservableLike<T> {
+    subscribe(subscriber: SubscribeFunction<T>): ISubscription
+}
+
+/**
+ * rxjs is an optional dependency that only needs installing when any of AceBase's observe methods are used.
+ * If for some reason rxjs is not available (eg in test suite), we can provide a shim. This class is used when
+ * `db.setObservable("shim")` is called
+ */
+ export class ObservableShim<T> implements IObservableLike<T> {
+    private _active: boolean = false;
+    private _create: CreateFunction<T>;
+    private _cleanup: CleanupFunction;
+    private _subscribers: SubscribeFunction<T>[] = [];
+    constructor (create: CreateFunction<T>) {
+        this._create = create;
+    }
+    subscribe(subscriber: SubscribeFunction<T>) {
+        if (!this._active) {
+            const next = (value: T) => {
+                // emit value to all subscribers
+                this._subscribers.forEach(s => {
+                    try { s(value); }
+                    catch(err) { console.error(`Error in subscriber callback:`, err); }
+                });
+            }
+            const observer:IObserver<T> = { next };
+            this._cleanup = this._create(observer);
+            this._active = true;
+        }
+        this._subscribers.push(subscriber);
+        const unsubscribe = () => {
+            this._subscribers.splice(this._subscribers.indexOf(subscriber), 1);
+            if (this._subscribers.length === 0) {
+                this._active = false;
+                this._cleanup();
+            }
+        }
+        const subscription:ISubscription = {
+            unsubscribe
+        };
+        return subscription;
+    }
 }

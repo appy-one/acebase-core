fd15977a373959fe7aaacac426e6bb5f8815d27a
Ewout Stortenbeker <4ewout@gmail.com>
2021-11-25 16:20:32 +0100

    Updated jsdoc for ObjectCollection.from

--- types/object-collection.d.ts
+++ types/object-collection.d.ts
@@ -15,8 +15,29 @@
 
 export class ObjectCollection<T> {
     /**
-     * Converts and array of values into an object collection
+     * Converts and array of values into an object collection, generating a unique key for each item in the array
      * @param array 
+     * @example
+     * const array = [
+     *  { title: "Don't make me think!", author: "Steve Krug" },
+     *  { title: "The tipping point", author: "Malcolm Gladwell" }
+     * ];
+     * 
+     * // Convert:
+     * const collection = ObjectCollection.from(array);
+     * // --> { 
+     * //   kh1x3ygb000120r7ipw6biln: { 
+     * //       title: "Don't make me think!", 
+     * //       author: "Steve Krug" 
+     * //   },
+     * //   kh1x3ygb000220r757ybpyec: { 
+     * //       title: "The tipping point", 
+     * //       author: "Malcolm Gladwell" 
+     * //   }
+     * // }
+     * 
+     * // Now it's easy to add them to the db:
+     * db.ref('books').update(collection);
      */
     static from<T>(array: T[]): IObjectCollection<T>
 }

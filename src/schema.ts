6fc802bdfa6be31968f85558006f481131658f2e
Ewout Stortenbeker <me@appy.one>
2023-02-13 20:49:15 +0100

    use ; as type delimiter

--- src/schema.ts
+++ src/schema.ts
@@ -1,18 +1,18 @@
 export interface IType {
-    typeOf: string, // typeof
+    typeOf: string; // typeof
     // eslint-disable-next-line @typescript-eslint/ban-types
-    instanceOf?: Function, // eg: instanceof 'Array'
-    value?:string|number|boolean|bigint|null,
-    genericTypes?: IType[],
-    children?: IProperty[],
-    matches?: RegExp // NEW: enforces regular expression checks on values
+    instanceOf?: Function; // eg: instanceof 'Array'
+    value?: string|number|boolean|bigint|null;
+    genericTypes?: IType[];
+    children?: IProperty[];
+    matches?: RegExp; // enforces regular expression checks on values
 }
 
 export interface IProperty {
-    name: string,
-    optional: boolean,
-    wildcard: boolean,
-    types: IType[]
+    name: string;
+    optional: boolean;
+    wildcard: boolean;
+    types: IType[];
 }
 
 // parses a typestring, creates checker functions
